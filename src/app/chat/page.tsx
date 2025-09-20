0df32bc191dcdfe06029d3e079c88f7ce2c44478
Chatri Ngambenchawong <pingkunga@gmail.com>
2025-09-20 21:15:02 +0700

    feat: simple chat

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,23 +1,63 @@
 'use client'
 
+import { useChat } from "@ai-sdk/react"
+import { DefaultChatTransport } from "ai"
 import { useState } from "react"
 
 function ChatPage() {
+
+    // useChat from 'ai/react'  // ตัวช่วยสร้าง chat interface (UI) แบบสำเร็จรูป
+    const { messages, sendMessage, status } = useChat({
+        transport: new DefaultChatTransport({
+            api: "/api/chat_04_stream",
+        }),
+    })
 
     //Store the chat input in a state variable
     const [input, setInput] = useState("")
 
     console.log("Render Chat Page:", input)
     return (
-        <div>
+        <div className="max-w-3xl mx-auto w-full mt-20">
             <h1>Chat Page</h1>
-            <form action="">
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                sendMessage({ text: input })
+                setInput("")
+            }}>
                 <input
                     type="text"
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
+                    className="border border-red-300 rounded-md p-2 w-96"
                 />
+                <button type="submit" className="ml-2 p-2 bg-blue-500 text-white rounded-md">
+                    Send
+                </button>
             </form>
+
+            {/* Show status */}
+            {
+            <div className="mt-4">
+                {status === "submitted" || status === "streaming"  && <div className="text-gray-500">Loading...</div>}
+                {status === "error" && <div className="text-red-500">Error sending message</div>}
+            </div>
+            }
+
+            {/* Display chat messages */}
+            {
+            <div className="mt-4">
+                {messages.map((msg, msgIndex) => (
+                    <div key={msgIndex} className={`flex ${msg.role === 'user' ? 'justify-end': 'justify-start'}`}>
+                        {msg.parts.map((part, index) => 
+                            part.type === 'text' ? (
+                                <div key={index} className="whitespace-pre-wrap"> {part.text}</div>
+                            ) : null
+                        )}
+                    </div>
+                ))}
+            </div>
+            }
         </div>
     )
 }
